0e4d4c597cf1c55a4044150b0d5f697f73c0aee6
Laura Messa Travin <laura.travin@gmail.com>
2021-03-29 20:58:50 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,12 @@
 class SessionsController < ApplicationController
+ def new
+ end
 
-  
+  def create
+        if params[:name].nil? || params[:name].empty?
+         # byebug
+         redirect_to :login
+        end
+  end
+
 end
